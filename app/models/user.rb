75b5a8600998458f0e474f9b59fc474dfed39519
tonycweng <tcweng91@gmail.com>
2017-08-06 14:46:29 -0700

    add model level validations and methods

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 8, allow_nil: true }
+  after_initialize :ensure_session_token
+  before_validation :ensure_session_token_uniqueness
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    ensure_session_token_uniqueness
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def ensure_session_token_uniqueness
+    while User.find_by(session_token: self.session_token)
+      self.session_token = new_session_token
+    end
+  end
+
+end
