ca1763eb45a98035ad2e4e7e4235aa49c3b51255
tonycweng <tcweng91@gmail.com>
2017-08-08 12:05:11 -0700

    create errors actions, reducer, root reducer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,7 +4,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
-    Hi There!
+    <h1>Hi There</h1>
   </div>
 );
 
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-import configureStoe from './store/store';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  window.store = store;
+  ReactDOM.render(<Root store={store} />, root);
 });
