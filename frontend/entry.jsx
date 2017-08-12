ce2b3b609aec71a893fcbf9a0aa43382ed3ab967
tonycweng <tcweng91@gmail.com>
2017-08-11 19:18:46 -0700

    add scss reset style sheet

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
+import { signup, login, logout } from './actions/session_actions';
+// import { signup, login, logout } from './util/session_api_util';
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
   if (window.currentUser) {
-    const preloadedState = { session: { currentUser: window.currentUser } };
+    const preloadedState = { session: window.currentUser };
     store = configureStore(preloadedState);
     delete window.currentUser;
   } else {
